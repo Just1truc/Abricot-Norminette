60f438449611fda80098a91dffd4296d97e67fa1
Ostentatoire <justin.duc@epitech.eu>
2021-12-19 23:26:37 +0100

    V.0.2 color added

--- brico.py
+++ brico.py
@@ -6,7 +6,7 @@ from os import path
 def check_include(files):
     for dos in files:
         if ((".h" in dos) != True):
-            print("[MAJOR]: [G6]: Include folder should only contain .h files:", dos)
+            print("\033[1;31;40m[MAJOR]: [G6]: Include folder should only contain .h files:", dos)
 
 def check_layout_inside_function(files):
     inside = open(files, "r")
@@ -17,7 +17,7 @@ def check_layout_inside_function(files):
         test += 1
         for char in lines:
             if (char == '\t' and ("Makefile" in files) != True):
-                print("[MINOR]: [L2]: No tab should be replaced by an identation:", files, "line :", test)
+                print("\033[1;33;40m[MINOR]: [L2]: No tab should be replaced by an identation:", files, "line :", test)
     inside.close()
             
 def check_function(files):
@@ -26,7 +26,7 @@ def check_function(files):
     for lines in inside:
         line += 1
         if len(lines) > 80:
-            print("[MAJOR]: [F3]: The length of a line should not exceed 80 columns:", files, "line :", line, "(", len(lines), "> 80 )")
+            print("\033[1;31;40m[MAJOR]: [F3]: The length of a line should not exceed 80 columns:", files, "line :", line, "(", len(lines), "> 80 )")
     inside.close()
     inside = open(files, "r")
     counter = 0
@@ -42,7 +42,7 @@ def check_function(files):
                 counter += 1
             if (lines[0] == '}'):
                 if (counter - 3 > 20):
-                    print("[MAJOR]: [F4]: A function should not exceed 20 lines:", files, "line :", begin_line, "(", counter - 3, "> 20 )")
+                    print("\033[1;31;40m[MAJOR]: [F4]: A function should not exceed 20 lines:", files, "line :", begin_line, "(", counter - 3, "> 20 )")
                 counter = 0
     inside.close()
     inside = open(files, "r")
@@ -57,7 +57,7 @@ def check_function(files):
                 counter += 1
             if (char == ')'):
                 if (counter > 4):
-                    print("[MAJOR]: [F5]: Function should not need more than 4 arguments:", files, "line :", line,"(", counter, "> 4 )")
+                    print("\033[1;31;40m[MAJOR]: [F5]: Function should not need more than 4 arguments:", files, "line :", line,"(", counter, "> 4 )")
     inside.close()
             
 def check_global_scope(files):
@@ -81,10 +81,10 @@ def check_global_scope(files):
         line_nbr += 1
     if (".c" in  files):
         if (result != "/*\n** EPITECH PROJECT,\n** File description:\n*/\n"):
-            print("[MAJOR]: [G1]: File header not correct:", files)
+            print("\033[1;31;40m[MAJOR]: [G1]: File header not correct:", files)
     if (files == "Makefile"):
         if (result != "##\n## EPITECH PROJECT,\n## File description:\n##\n"):
-            print("[MAJOR]: [G1]: File header not correct:", files)
+            print("\033[1;31;40m[MAJOR]: [G1]: File header not correct:", files)
     inside.close()
     inside = open(files, "r")
     trailling_lines = 0;
@@ -98,7 +98,7 @@ def check_global_scope(files):
                 trailling_lines = 0
             if (trailling_lines == 2):
                 trailling_lines = 0;
-                print("[MINOR]: [G2]: There should be only one empty_line between implementations:", files, ": line:", line_nbr)
+                print("\033[1;33;40m[MINOR]: [G2]: There should be only one empty_line between implementations:", files, ": line:", line_nbr)
     inside.close()
     inside = open(files, "r")
     line = 0
@@ -107,7 +107,7 @@ def check_global_scope(files):
         index = 0
         for char in lines:
             if (char == ' ' and lines[index + 1] == '\n' and line > 7):
-                print("[MINOR]: [G8]: Trailling space:", files, "line :", line)
+                print("\033[1;33;40m[MINOR]: [G8]: Trailling space:", files, "line :", line)
             index += 1
     inside.close()
             
@@ -115,9 +115,9 @@ def check_file_organization(files):
     forbidden_files = [ ".o", ".gch", ".a", ".so", "~", "#", ".d" ]
     for ext in forbidden_files:
         if (ext in str(files)):
-            print("[MAJOR]: [O1]: Delivery Folder should not contain", ext,"files:", files)
+            print("\033[1;31;40m[MAJOR]: [O1]: Delivery Folder should not contain", ext,"files:", files)
     if (any(ele.isupper() for ele in str(files)) == True and ("Makefile" in files) != True):
-        print("[MAJOR]: [O4]: Name not in snake case convention:", files)
+        print("\033[1;31;40m[MAJOR]: [O4]: Name not in snake case convention:", files)
 
 def check_coding_style(files):
     check_file_organization(files)
