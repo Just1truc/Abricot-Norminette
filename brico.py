955e27400a4e6eaf4199546109783304eaf55c50
Ostentatoire <justin.duc@epitech.eu>
2022-01-24 15:19:40 +0100

    V.3.2 Plaban fixed

--- brico.py
+++ brico.py
@@ -255,7 +255,7 @@ def check_coding_style(files):
     major = []
     minor = []
     check_file_organization(files)
-    if ((files[-1] == 'c' and files[-2] == '.') or "Makefile" in files or (files[-1] == 'h' and files[-2] == '.')) and not("~" in files):
+    if ((files[-1] == 'c' and files[-2] == '.') or "Makefile" in files or (files[-1] == 'h' and files[-2] == '.')) and not("~" in files) and not(".swp" in files):
         check_global_scope(files)
         check_function(files)
         check_layout_inside_function(files)
