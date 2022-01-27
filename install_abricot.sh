a6221e7f572c722be3764823ad9e23b3d60231ae
Ostentatoire <justin.duc@epitech.eu>
2022-01-27 18:52:00 +0100

    V.5.1 Passage sous path

--- install_abricot.sh
+++ install_abricot.sh
@@ -6,6 +6,11 @@ then
     sudo rm -rf ~/.Abricot-script/
 fi
 tput setaf 2
+FILE=/usr/local/lib/Abricot_scripts
+if [ -d $FILE ]
+then
+    sudo rm -rf /usr/local/lib/Abricot_scripts
+fi
 echo "=> Erasing Done"
 tput sgr 0
 echo "=> Installing clang-format..."
@@ -16,25 +21,28 @@ sudo zypper -y install clang-tools-extra 2> /dev/null
 tput setaf 2
 echo "=> Installation of clang done"
 tput init
-echo "=> Adding necessary file to a proper functionning..."
-mkdir ~/.Abricot-script
-tput setaf 2
-echo "=> Done adding"
-tput sgr 0
 echo "=> Copying source code..."
-cp -R . ~/.Abricot-script/
+sudo mkdir /usr/local/lib/Abricot_scripts
+sudo cp -R . /usr/local/lib/Abricot_scripts
 tput setaf 2
 echo "=> Done copying source code"
 tput sgr 0
-echo "=> Adding zsh alias if needed..."
+echo "=> Erasing zsh alias if needed..."
 OUTPUT=$(sudo cat ~/.zshrc | grep "alias abricot")
-if [[ $OUTPUT == "" ]]
+if [[ $OUTPUT != "" ]]
 then
-    echo "alias abricot='sh ~/.Abricot-script/brico.sh'" >> ~/.zshrc
+    word="alias abricot='sh ~/.Abricot-script/brico.sh'"
+    vide=""
+    sed 's/$word/$vide/g' ~/.zshrc > ~/.zshrc
     tput setaf 2
-    echo "=> Alias added"
+    echo "=> Alias erased"
     tput sgr 0
 fi
+echo "=> Copying source code into bin..."
+sudo cp abricot /usr/local/bin
+tput setaf 2
+echo "=> Done copying"
+tput sgr0
 tput setaf 2
 echo "=> Installation Done"
 tput sgr 0
