ff4d91ddc8550e6a89b46552053e210cb424c9ce
MathiDEV <mathias74.andre@gmail.com>
2022-10-17 12:13:27 +0200

    :zap: Making G3 faster

--- src/rules/C_G3.py
+++ src/rules/C_G3.py
@@ -44,7 +44,7 @@ def _is_pp_directive(file: str, line_number: int, directives: List[str]):
 
 def checker(config):
     for file in abricot.getSourceFileNames(config):
-        if not is_source_file(file) and not is_header_file(file):
+        if not is_header_file(file):
             continue
 
         lines = get_lines(file)
