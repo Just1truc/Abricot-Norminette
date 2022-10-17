9f874a7741ba246ecff3ad19c1c87f2f7cde49a5
MathiDEV <mathias74.andre@gmail.com>
2022-10-17 13:54:11 +0200

    :zap: Custom error if is not a git repository

--- src/program/ignored.py
+++ src/program/ignored.py
@@ -1,12 +1,20 @@
 import os
 
+from program.print import Colors, printc
+
 def getIgnoredFiles():
     try:
-        os.system("git check-ignore $(find . -type f -print) > .abricotignore")
+        res = os.system("git check-ignore $(find . -type f -print) > .abricotignore 2> /dev/null")
+        if res != 0:
+            printc("ERROR: This directory is not a git repository...", bold=True, color=Colors.RED)
+            os.system("rm .abricotignore")
+            raise FileNotFoundError()
         with open(".abricotignore", "r") as f:
             ignored = f.readlines()
         os.system("rm .abricotignore")
         return [x[2:].replace("\n", "") if x.startswith("./") else x.replace("\n", "") for x in ignored]
+    except FileNotFoundError:
+        exit(1)
     except:
         return []
     
