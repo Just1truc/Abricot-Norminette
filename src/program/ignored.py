6605cb68beedaa7fd3a829ee6193aad8e65ad729
MathiDEV <mathias74.andre@gmail.com>
2022-10-17 18:15:16 +0200

    :zap: Le github

--- src/program/ignored.py
+++ src/program/ignored.py
@@ -1,20 +1,13 @@
 import os
 
-from program.print import Colors, printc
 
 def getIgnoredFiles():
     try:
-        res = os.system("git check-ignore $(find . -type f -print) > .abricotignore 2> /dev/null")
-        if res != 0:
-            printc("ERROR: This directory is not a git repository...", bold=True, color=Colors.RED)
-            os.system("rm .abricotignore")
-            raise FileNotFoundError()
+        os.system("git check-ignore $(find . -type f -print) > .abricotignore 2> /dev/null")
         with open(".abricotignore", "r") as f:
             ignored = f.readlines()
         os.system("rm .abricotignore")
         return [x[2:].replace("\n", "") if x.startswith("./") else x.replace("\n", "") for x in ignored]
-    except FileNotFoundError:
-        exit(1)
     except:
         return []
     
