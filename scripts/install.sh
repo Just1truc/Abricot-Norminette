39ae905b236df135d56011b02afd7bbce413a20d
Just1truc <justin.duc@epitech.eu>
2022-10-29 16:40:37 +0200

    [UPDATE] : Rebase on main

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,14 +23,42 @@ tput sgr0
 
 # if clang-format is not installed
 if ! [ -x "$(command -v clang-format)" ]; then
-    tput sgr 0
-    echo "=> Installing clang-format..."
-    sudo dnf -y install clang-tools-extra pip &> /dev/null
-    sudo apt update; sudo apt -y install clang-format pip &> /dev/null
-    sudo zypper -y install clang-tools-extra pip &> /dev/null
-    sudo pacman -Sy clang-tools-extra &> /dev/null
-    tput setaf 2
-    echo "=> Installation of clang done"
+    if ! [ "$(uname)" == "Darwin" ]; then
+        tput sgr 0
+        echo "=> Installing clang-format..."
+        if [ -x "$(command -v dnf)" ]; then
+            sudo dnf install clang-tools-extra pip || (tput setaf 1; echo "=> Error: clang-tools-extra install went wrong"; tput sgr0; exit 1)
+        elif [ -x "$(command -v apt-get)" ]; then
+            sudo apt-get install clang-format || (tput setaf 1; echo "=> Error: clang-format install went wrong"; tput sgr0; exit 1)
+        elif [ -x "$(command -v zypper)" ]; then
+            sudo zypper install clang-format || (tput setaf 1; echo "=> Error: clang-format install went wrong"; tput sgr0; exit 1)
+        elif [ -x "$(command -v pacman)" ]; then
+            sudo pacman -S clang || (tput setaf 1; echo "=> Error: clang-format install went wrong"; tput sgr0; exit 1)
+        else
+            tput setaf 1
+            echo "=> Error: Your distribution is not supported"
+            tput sgr0
+            exit 1
+        fi
+        echo "=> Installation of clang done"
+    else if [ "$(uname -s)" == "Darwin" ]; then
+        if ! [ -x "$(command -v brew)" ]; then
+            tput sgr 0
+            echo "=> Brew is not installed"
+            echo "=> Installing brew..."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+            tput setaf 2
+            echo "=> Brew installed"
+            tput sgr0
+        fi
+        tput setaf 0
+        echo "=> Installing clang-format..."
+        brew install clang-format
+        tput setaf 2
+        echo "=> Installation of clang done"
+        tput sgr0
+    fi
+fi
 else
     tput setaf 2
     echo "=> clang-format found"
