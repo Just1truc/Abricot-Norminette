7a2e89f927a10a0f077dd8d74b34d00101657660
MathiDEV <mathias74.andre@gmail.com>
2022-11-05 17:15:54 +0100

    :zap: Check version on master

--- src/program/updater.py
+++ src/program/updater.py
@@ -3,7 +3,7 @@ import requests
 from program.print import printc, Colors
 from __init__ import __version__
 
-URL_BASE = "https://raw.githubusercontent.com/Just1truc/Abricot-Norminette"
+URL_BASE = "https://raw.githubusercontent.com/Just1truc/Abricot-Norminette/main"
 VERSION_FILE = "%s/VERSION" % URL_BASE
 UPDATE_FILE = "%s/get_abricot.sh" % URL_BASE
 
