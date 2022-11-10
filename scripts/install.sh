7dcd20a3873b99058bb7535a050fa9cea60d272c
MathiDEV <mathias74.andre@gmail.com>
2022-11-10 11:05:24 +0100

    :zap: pip in installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,8 +21,8 @@ echo "=> Done erasing"
 tput sgr0
 
 
-# if clang-format is not installed
-if ! [ -x "$(command -v clang-format)" ]; then
+# if clang-format or pip is not installed
+if ! [ -x "$(command -v clang-format)" || -x "$(command -v pip)" ]; then
     if ! [ "$(uname)" == "Darwin" ]; then
         tput sgr 0
         echo "=> Installing clang-format..."
